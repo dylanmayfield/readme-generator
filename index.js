efa2d172f5c7f527c1867167ccb72b09f89def50
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-10-10 23:12:40 -0500

    udpated generateMarkdown.js and index.js

--- index.js
+++ index.js
@@ -1,87 +1,75 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs')
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'name', 
-        message: 'What is the name of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description', 
-        message: 'Describe your project.'
-    },
-    {
-        type: 'input',
-        name: 'installation', 
-        message: 'What are the installation instructions of your project?'
-    },
-    {
-        type: 'input',
-        name: 'usage', 
-        message: 'What is the usage information on your project?'
-    },
-    {
-        type: 'input',
-        name: 'contributing', 
-        message: 'What are the Contribution Guidelines of your project?'
-    },
-    {
-        type: 'input',
-        name: 'test', 
-        message: 'What are the test instructions for your project?'
-    },
-];
+// const questions = [];
 
-// inquirer
-//     .prompt([
-//         {
-//             type: 'input',
-//             name: 'name', 
-//             message: 'What is the name of your project?'
-//         },
-//         {
-//             type: 'input',
-//             name: 'description', 
-//             message: 'Describe your project.'
-//         },
-//         {
-//             type: 'input',
-//             name: 'installation', 
-//             message: 'What are the installation instructions of your project?'
-//         },
-//         {
-//             type: 'input',
-//             name: 'usage', 
-//             message: 'What is the usage information on your project?'
-//         },
-//         {
-//             type: 'input',
-//             name: 'contributing', 
-//             message: 'What are the Contribution Guidelines of your project?'
-//         },
-//         {
-//             type: 'input',
-//             name: 'test', 
-//             message: 'What are the test instructions for your project?'
-//         },
-//     ])
 
 // // TODO: Create a function to write README file
-function writeToFile(README, data) {
-fs.writeFile('README.md', data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!')
-}
-)};
+function writeToFile(fileType, data) {
+    fs.writeFile(fileType, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!')
+    }
+    )
+};
 
 // // TODO: Create a function to initialize app
 function init() {
+    // inquirer.prompt(questions).then((answers) => {
+    //     console.log(answers)
+    //     writeToFile('README.md', generateMarkdown(answers))
+    // })
+    
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe your project.'
+        },
+        {
+            type: 'input',
+            name: 'table',
+            message: 'What is the table of contents of your project?'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation instructions of your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage information on your project?'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'What are the Contribution Guidelines of your project?'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What are the test instructions for your project?'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What are the questions for your project?'
+        },
+    ]).then((answers) => {
+        console.log(answers)
+        writeToFile('README.md', generateMarkdown(answers))
+    })
 
-}
-
+};
 // // Function call to initialize app
-// init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if (license !== 'None') {
+      return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  } else {
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  if (license !== 'None') {
+      return `\n* [License](#license)\n`;
+  } else {
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+if (license !== 'None') {
+    return `## License 
+    This project is licensed under the ${license} license.`;
+ }};
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
+  //write code here
+  var dataName =`# ${data.name}
 
-`;
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  ${data.table}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Test
+
+  ${data.test}
+
+  ## Questions
+
+  ${data.questions}`
+  return dataName; 
+
+  renderLicenseBadge();
+renderLicenseLink();
+renderLicenseSection();
+
 }
 
 module.exports = generateMarkdown;
